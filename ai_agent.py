fb3f81de9c84074f3e6cd3280b40a6eb2f37617d
TanyaVaish-17 <tanyavaish05@gmail.com>
2025-10-01 20:17:26 +0530

    Add Tavily API integration

--- ai_agent.py
+++ ai_agent.py
@@ -1,7 +1,13 @@
 # phase 1:
 # 1. setup api keys for Groq and Tavily
-# 2. setup llm and tools
-# 3. setup ai agent with search tool functionality
 
 import os
 GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+
+
+
+
+# 2. setup llm and tools
+# 3. setup ai agent with search tool functionality
+
