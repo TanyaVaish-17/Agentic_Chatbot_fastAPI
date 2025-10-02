04742c710894ed36739debf2aabe9aa689bff5fd
TanyaVaish-17 <tanyavaish05@gmail.com>
2025-10-02 19:21:56 +0530

    Set up ai agent for frontend request

--- ai_agent.py
+++ ai_agent.py
@@ -17,8 +17,6 @@ from langchain_tavily import TavilySearch
 openai_llm = ChatOpenAI(model="gpt-4o-mini")
 groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
 
-search_tool = TavilySearch(max_results=2)  
-
 # 3. setup ai agent with search tool functionality
 
 from langgraph.prebuilt import create_react_agent
@@ -26,15 +24,21 @@ from langchain_core.messages.ai import AIMessage
 from langchain_core.messages import HumanMessage
 
 system_prompt = "Act as an AI Chatbot who is smart and friendly"
-agent = create_react_agent(
-    model=groq_llm,
-    tools=[search_tool],
-    prompt=system_prompt
-)
 
-query = "Tell me about the trends in crypto market"
-state = {"messages": [HumanMessage(content=query)]}
-response = agent.invoke(state)
-messages = response.get("messages")
-ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
-print(ai_messages[-1])
+def get_response_from_ai_agent(llm_id,query,allow_search,system_prompt,provider):
+    if provider=="Groq":
+        llm=ChatGroq(model=llm_id)
+    elif provider=="OpenAI":
+        llm=ChatOpenAI(model=llm_id)
+    tools = [TavilySearch(max_results=2)] if allow search else []
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        prompt=system_prompt
+    )
+    # query = "Tell me about the trends in crypto market"
+    state = {"messages": [HumanMessage(content=query)]}
+    response = agent.invoke(state)
+    messages = response.get("messages")
+    ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+    return (ai_messages[-1])
--- backend.py
+++ backend.py
@@ -11,10 +11,32 @@ class RequestState(BaseModel):                     # It's basically for frontend
 
 
 # 2. Setup ai agent from frontend request
+from fastapi import FASTAPI
+from ai_agent import get_response_from_ai_agent
 
+ALLOWED_MODEL_NAMES=["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
 
+#Now we have to set up our app for backend to manage the frontend request
+app=FastAPI(title="Langchain AI Agent")
 
+@app.post("/chat")  # it is the end point where the frontend request is received
 
+def chat_endpoint(request:RequestState):  # Request processing and also the the request received must be of class RequestState type
+     if request.model_name not in ALLOWED_MODEL_NAMES:
+        return {"error": "Invalid model name. Kindly select a valid AI model"}
 
+    # These all we will get from frontend
+
+    llm_id=request.model_name
+    query=request.messages
+    allow_search=request.allow_search
+    system_prompt=request.system_prompt
+    provider=request.model_provider
+
+    # Creating AI agent to get response from it
+    response=get_response_from_ai_agent(llm_id,query,allow_search,system_prompt,provider)
+    return response
 
 # 3. Run app and explore swagger ui docs
+
+# Now we have to host it using uvicorn
